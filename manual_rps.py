4787d712b7a04dc7311d688404a0267b74655aca
ichaize <isabellechaize1@gmail.com>
2023-04-12 21:03:25 +0100

    Adds a get_winner function to determine who wins

--- manual_rps.py
+++ manual_rps.py
@@ -8,3 +8,25 @@ def get_computer_choice():
 def get_user_choice():
     user_choice = input("Pick rock, paper or scissors: ")
     return user_choice
+
+def get_winner(computer_choice, user_choice):
+    if computer_choice == user_choice:
+        print("It is a tie!")
+    elif computer_choice == "Rock":
+        if user_choice == "Paper":
+            print("You win!")
+        else:
+            print("You lost")
+    elif computer_choice == "Paper":
+        if user_choice == "Scissors":
+            print("You win!")
+        else:
+            print("You lost")
+    elif computer_choice == "Scissors":
+        if user_choice == "Rock":
+            print("You win!")
+        else:
+            print("You lost")
+
+
+    

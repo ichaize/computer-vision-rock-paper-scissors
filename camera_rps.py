fb7f6ec4f4638ba0d14e0533259dbc1116d7227e
ichaize <isabellechaize1@gmail.com>
2023-04-20 15:00:29 +0100

    latest version

--- camera_rps.py
+++ camera_rps.py
@@ -11,6 +11,8 @@ class RockPaperScissors:
     def __init__(self):
         self.computer_wins = 0
         self.user_wins = 0
+        self.move_list = ["Rock", "Paper", "Scissors", "Nothing"]
+        self.result = None
 
     def countdown(self):
         print("Counting down: 3...")
@@ -21,23 +23,16 @@ class RockPaperScissors:
         time.sleep(1)
 
     def run_model(self):
-        self.countdown()
-        while True: 
+        start_time = time.time()
+        while time.time() < start_time + 5:
             ret, frame = cap.read()
             resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
             image_np = np.array(resized_frame)
             normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
             data[0] = normalized_image
-            prediction = model.predict(data)
+            prediction = np.argmax(model.predict(data))
+            self.result = self.move_list[prediction]
             cv2.imshow('frame', frame)
-            winner = self.get_winner(self.get_computer_choice(), self.get_prediction(prediction))
-            self.update_score(winner)
-            if game_one.computer_wins == 3:
-                print("The computer won!")
-                break
-            elif game_one.user_wins == 3:
-                print("You won!")
-                break
             # Press q to close the window
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break             
@@ -45,17 +40,6 @@ class RockPaperScissors:
         cap.release()
         # Destroy all the windows
         cv2.destroyAllWindows()
-        
-    def get_prediction(self, prediction):
-        highest_probability_index = np.argmax(prediction)
-        if highest_probability_index == 0:
-            return "Rock"
-        elif highest_probability_index == 1:
-            return "Paper"
-        elif highest_probability_index == 2:
-            return "Scissors"
-        else:
-            return "Nothing"
 
     def get_computer_choice(self):
         options = ["Rock", "Paper", "Scissors"]
@@ -87,17 +71,31 @@ class RockPaperScissors:
             else:
                 print("You lost")
                 return "computer"
-
-    def update_score(self, winner): 
+    
+    def play(self):
+            computer_choice = self.get_computer_choice()
+            self.run_model()
+            winner = self.get_winner(computer_choice, self.result) 
             if winner == "user":
                 self.user_wins += 1
             elif winner == "computer":
                 self.computer_wins += 1
             print(self.computer_wins, self.user_wins)
-            self.countdown()
+        
+
+            
 
 game_one = RockPaperScissors()
-game_one.run_model()
+while True:
+    if game_one.computer_wins == 3:
+            print("The computer won!")
+            break
+    elif game_one.user_wins == 3:
+            print("You won!")
+            break
+    else:
+        game_one.play()
+        time.sleep(5)
 
 """ def check_keep_playing():
     if c_wins == 3:

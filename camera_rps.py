14a3dd49e01536ebae4254a5aee5b71f81d0d7d1
ichaize <isabellechaize1@gmail.com>
2023-04-20 14:07:08 +0100

    Putting gameplay inside the run_model method

--- camera_rps.py
+++ camera_rps.py
@@ -12,7 +12,7 @@ class RockPaperScissors:
         self.computer_wins = 0
         self.user_wins = 0
 
-    def alt_timer(self):
+    def countdown(self):
         print("Counting down: 3...")
         time.sleep(1)
         print("2...")
@@ -20,18 +20,8 @@ class RockPaperScissors:
         print("1...")
         time.sleep(1)
 
-    @classmethod
-    def timer():
-        start_time = time.time()
-        while (time.time() - start_time) < 1:
-            print("Counting down: 3")
-        while (time.time() - start_time) < 2:
-            print("Counting down: 2")
-        while (time.time() - start_time) < 3:
-            print("Counting down: 1")
-
     def run_model(self):
-        self.alt_timer()
+        self.countdown()
         while True: 
             ret, frame = cap.read()
             resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
@@ -40,6 +30,14 @@ class RockPaperScissors:
             data[0] = normalized_image
             prediction = model.predict(data)
             cv2.imshow('frame', frame)
+            winner = self.get_winner(self.get_computer_choice(), self.get_prediction(prediction))
+            self.update_score(winner)
+            if game_one.computer_wins == 3:
+                print("The computer won!")
+                break
+            elif game_one.user_wins == 3:
+                print("You won!")
+                break
             # Press q to close the window
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break             
@@ -47,10 +45,9 @@ class RockPaperScissors:
         cap.release()
         # Destroy all the windows
         cv2.destroyAllWindows()
-        return prediction
-
-    def get_prediction(self):
-        highest_probability_index = np.argmax(self.run_model())
+        
+    def get_prediction(self, prediction):
+        highest_probability_index = np.argmax(prediction)
         if highest_probability_index == 0:
             return "Rock"
         elif highest_probability_index == 1:
@@ -66,7 +63,6 @@ class RockPaperScissors:
         print(f"The computer chose {computer_choice}")
         return computer_choice
         
-    ### for some reason the printed bit of this (e.g. "You lost" prints twice each round, I have no idea why. The scores in play() only print once ###    
     def get_winner(self, computer_choice, user_choice):
         if computer_choice == user_choice:
             print("It is a tie!")
@@ -92,26 +88,16 @@ class RockPaperScissors:
                 print("You lost")
                 return "computer"
 
-    def play(self):
-            computer_choice = self.get_computer_choice()
-            user_choice = self.get_prediction()
-            winner = self.get_winner(computer_choice, user_choice) 
+    def update_score(self, winner): 
             if winner == "user":
                 self.user_wins += 1
             elif winner == "computer":
                 self.computer_wins += 1
             print(self.computer_wins, self.user_wins)
+            self.countdown()
 
 game_one = RockPaperScissors()
-while True:
-    if game_one.computer_wins == 3:
-            print("The computer won!")
-            break
-    elif game_one.user_wins == 3:
-            print("You won!")
-            break
-    else:
-        game_one.play()
+game_one.run_model()
 
 """ def check_keep_playing():
     if c_wins == 3:
